b09b758532f781b2261ee490bf3ee9fdd15a4cec
HarshOB <harshp@officebrain,com>
2017-06-06 15:36:17 +0530

    Final Commit

---
+++ src/components/SignIn.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="signip">
+	<div class="signup-page">
+		<component :is="top"></component>
+	    <component :is="signin"></component>
+		<component :is="down"></component>
+	</div>
+</div>
+</template>
+
+<script>
+	import Top from './indexMain/Top'
+	import Down from './indexMain/Down'
+	import SignInMain from './indexMain/SignInMain'
+	export default {
+		name: 'Index',
+		components: {
+			Top, Down, SignInMain
+		},
+		data() {
+			return {
+				top: 'Top',
+				down: 'Down',
+				signin: 'SignInMain'
+			}
+		}
+	}
+</script>
---
+++ src/components/dashMain/newfeedback/FormBuilder.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="formbuilder">
+    <div class="row">
+      <div class="col-md-12">
+        <div id="build-wrap"></div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <a href="#" id="prev-general" class="btn btn-default pull-left">Previous</a>
+        <a href="#" id="next-final" class="btn btn-primary pull-right">Next</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FormBuilder',
+     mounted: function () {
+      jQuery(function($) {
+        var fbTemplate = document.getElementById('build-wrap');
+        $(fbTemplate).formBuilder();
+      });
+    }
+  }
+</script>
